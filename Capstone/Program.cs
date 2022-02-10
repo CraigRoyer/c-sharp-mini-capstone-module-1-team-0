6e0bc24a47416fbb94f7f82b121122966c600736
Jennifer Hall <j.m.hall369@gmail.com>
2022-02-10 12:20:00 -0500

    vending machine first commit

--- Capstone/Program.cs
+++ Capstone/Program.cs
@@ -6,7 +6,28 @@ namespace Capstone
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int selector = 0;
+            while (selector != 3)
+            {
+                Console.WriteLine($"Hello Patron! Please enter" +
+                    $"{Environment.NewLine}(1)Display Vending Machine Items" +
+                    $"{Environment.NewLine}(2)Purchase" +
+                    $"{Environment.NewLine}(3)Exit");
+
+                string selection = Console.ReadLine();
+
+                while (!int.TryParse(selection, out int num))
+                {
+                    Console.WriteLine($"Please enter a number between 1 - 3");
+                }
+               selector = int.Parse(selection);
+            }
+
+           
+
+
+            
+
         }
     }
 }
